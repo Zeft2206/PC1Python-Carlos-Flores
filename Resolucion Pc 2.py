deedfd08119c137929a416397ba59aa218f73f6a
Zeft2206 <157268914+Zeft2206@users.noreply.github.com>
2024-02-02 10:23:55 -0500

    Add files via upload

---
+++ Resolucion Pc 2.py
@@ -0,0 +1,17 @@
+# Ejercicio 1
+numero = int(1500)
+divisor = int(35)
+max = int(2700)
+min = int(1500)
+print('Los números multiplos de 5 y divisibles por 7 son:' )
+while numero <= max:
+    if numero % divisor == 0:
+        print(f(numero))
+numero = numero + 1
+
+
+
+
+
+
+
